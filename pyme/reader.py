4263f77544dcad80e506b2e0364eb59525e80262
Dmitry Grigoryev <dmitriy57@gmail.com>
2018-03-14 01:26:39 -0500

    Refactored reader into a class

--- pyme/reader.py
+++ pyme/reader.py
@@ -34,130 +34,124 @@ def to_digit(char):
     return char_to_digit.get(char)
 
 
-def _read_list(port):
-    result = []
-    while True:
-        item = _read(port)
-        if isinstance(item, _RightBracket):
-            return interop.scheme_list(result)
-        elif core.is_eof(item):
+class Reader:
+
+    def _read_list(self, port):
+        result = []
+        while True:
+            item = self._read(port)
+            if isinstance(item, _RightBracket):
+                return interop.scheme_list(result)
+            elif core.is_eof(item):
+                raise exceptions.ReaderError('Unexpected end of file.')
+            else:
+                result.append(item)
+
+    def _read_string(self, port):
+        result = ""
+        while True:
+            item = port.peek_char()
+            if core.is_eof(item):
+                raise exceptions.ReaderError('Unexpected end of file.')
+            elif item == '"':
+                port.read(1)
+                return result
+            else:
+                port.read(1)
+                result += item
+
+    def _slice_symbol_or_number(self, port):
+        result = ""
+        while True:
+            item = port.peek_char()
+            if core.is_eof(item) or not is_symbol_char(item):
+                return result
+            else:
+                port.read(1)
+                result += item
+
+    def  _parse_integer(self, string):
+        sign = 1
+        if string[0] == "+":
+            string = string[1:]
+        elif string[0] == "-":
+            sign = -1
+            string = string[1:]
+        if len(string) == 0: return None
+        result = 0
+        for char in string:
+            digit = to_digit(char)
+            if digit is None: return None
+            result = result*10 + digit
+        return sign * result
+
+    def _get_symbol_or_number(self, string):
+        if string == ".":
+            raise exceptions.ReaderError("Illegal use of `.'")
+        as_integer = self._parse_integer(string)
+        if as_integer is not None:
+            return as_integer
+        return core.Symbol(string)
+
+    def _read_quoted(self, port):
+        quoted = self._read(port)
+        if isinstance(quoted, _RightBracket):
+            raise exceptions.ReaderError('Unexpected ")".')
+        if isinstance(quoted, core.Eof):
             raise exceptions.ReaderError('Unexpected end of file.')
-        else:
-            result.append(item)
+        return interop.scheme_list([core.Symbol("quote"), quoted])
 
-
-def _read_string(port):
-    result = ""
-    while True:
-        item = port.peek_char()
-        if core.is_eof(item):
-            raise exceptions.ReaderError('Unexpected end of file.')
-        elif item == '"':
-            port.read(1)
-            return result
-        else:
-            port.read(1)
-            result += item
-
-
-def _slice_symbol_or_number(port):
-    result = ""
-    while True:
-        item = port.peek_char()
-        if core.is_eof(item) or not is_symbol_char(item):
-            return result
-        else:
-            port.read(1)
-            result += item
-
-
-def  _parse_integer(string):
-    sign = 1
-    if string[0] == "+":
-        string = string[1:]
-    elif string[0] == "-":
-        sign = -1
-        string = string[1:]
-    if len(string) == 0: return None
-    result = 0
-    for char in string:
-        digit = to_digit(char)
-        if digit is None: return None
-        result = result*10 + digit
-    return sign * result
-
-
-def _get_symbol_or_number(string):
-    if string == ".":
-        raise exceptions.ReaderError("Illegal use of `.'")
-    as_integer = _parse_integer(string)
-    if as_integer is not None:
-        return as_integer
-    return core.Symbol(string)
-
-
-def _read_quoted(port):
-    quoted = _read(port)
-    if isinstance(quoted, _RightBracket):
-        raise exceptions.ReaderError('Unexpected ")".')
-    if isinstance(quoted, core.Eof):
-        raise exceptions.ReaderError('Unexpected end of file.')
-    return interop.scheme_list([core.Symbol("quote"), quoted])
-
-
-def _read_special(port):
-    char = port.peek_char()
-    if isinstance(char, core.Eof):
-        raise exceptions.ReaderError('Unexpected end of file.')
-    elif char == 't':
-        port.read(1)
-        next_char = port.peek_char()
-        if not core.is_eof(next_char) and is_symbol_char(next_char):
-            raise exceptions.ReaderError("Invalid hash syntax: #"
-                                         + char + next_char)
-        return True
-    elif char == 'f':
-        port.read(1)
-        next_char = port.peek_char()
-        if not core.is_eof(next_char) and is_symbol_char(next_char):
-            raise exceptions.ReaderError("Invalid hash syntax: #"
-                                         + char + next_char)
-        return False
-    else:
-        raise exceptions.ReaderError("Invalid hash syntax: #" + char)
-
-
-def _read(port):
-    while True:
+    def _read_special(self, port):
         char = port.peek_char()
         if isinstance(char, core.Eof):
-            return char
-        elif is_white(char):
+            raise exceptions.ReaderError('Unexpected end of file.')
+        elif char == 't':
             port.read(1)
-        elif char == ';':
-            port.readline()
-        elif char == '(':
+            next_char = port.peek_char()
+            if not core.is_eof(next_char) and is_symbol_char(next_char):
+                raise exceptions.ReaderError("Invalid hash syntax: #"
+                                             + char + next_char)
+            return True
+        elif char == 'f':
             port.read(1)
-            return _read_list(port)
-        elif char == ')':
-            port.read(1)
-            return _RightBracket.instance
-        elif char == '"':
-            port.read(1)
-            return _read_string(port)
-        elif char == "'":
-            port.read(1)
-            return _read_quoted(port)
-        elif char == "#":
-            port.read(1)
-            return _read_special(port)
+            next_char = port.peek_char()
+            if not core.is_eof(next_char) and is_symbol_char(next_char):
+                raise exceptions.ReaderError("Invalid hash syntax: #"
+                                             + char + next_char)
+            return False
         else:
-            string = _slice_symbol_or_number(port)
-            return _get_symbol_or_number(string)
+            raise exceptions.ReaderError("Invalid hash syntax: #" + char)
 
+    def _read(self, port):
+        while True:
+            char = port.peek_char()
+            if isinstance(char, core.Eof):
+                return char
+            elif is_white(char):
+                port.read(1)
+            elif char == ';':
+                port.readline()
+            elif char == '(':
+                port.read(1)
+                return self._read_list(port)
+            elif char == ')':
+                port.read(1)
+                return _RightBracket.instance
+            elif char == '"':
+                port.read(1)
+                return self._read_string(port)
+            elif char == "'":
+                port.read(1)
+                return self._read_quoted(port)
+            elif char == "#":
+                port.read(1)
+                return self._read_special(port)
+            else:
+                string = self._slice_symbol_or_number(port)
+                return self._get_symbol_or_number(string)
 
-def read(port):
-    result = _read(port)
-    if isinstance(result, _RightBracket):
-        raise exceptions.ReaderError('Unexpected ")".')
-    return result
+    def read(self, port):
+        result = self._read(port)
+        if isinstance(result, _RightBracket):
+            raise exceptions.ReaderError('Unexpected ")".')
+        return result
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -9,141 +9,145 @@ class TestReader(unittest.TestCase):
     def setUp(self):
         self.stream = io.StringIO()
         self.port = ports.TextStreamPort(self.stream)
+        self.reader = reader.Reader()
 
     def test_empty(self):
         in_port = ports.TextStreamPort(io.StringIO(""))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Eof)
 
     def test_whitespace(self):
         in_port = ports.TextStreamPort(io.StringIO("\t \n"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Eof)
 
     def test_comment(self):
         in_port = ports.TextStreamPort(io.StringIO(" ;hello\n"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Eof)
 
     def test_empty_list(self):
         in_port = ports.TextStreamPort(io.StringIO("()"))
-        core.write_to(reader.read(in_port), self.port)
+        core.write_to(self.reader.read(in_port), self.port)
         self.assertEqual(self.stream.getvalue(), "()")
 
     def test_string(self):
         in_port = ports.TextStreamPort(io.StringIO('"abc"'))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertEqual(result, "abc")
 
     def test_symbol(self):
         in_port = ports.TextStreamPort(io.StringIO("abc"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_space(self):
         in_port = ports.TextStreamPort(io.StringIO(" abc "))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_left_bracket(self):
         in_port = ports.TextStreamPort(io.StringIO("abc("))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_right_bracket(self):
         in_port = ports.TextStreamPort(io.StringIO("abc)"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_quote(self):
         in_port = ports.TextStreamPort(io.StringIO("abc'"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_double_quote(self):
         in_port = ports.TextStreamPort(io.StringIO('abc"'))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_symbol_semicolon(self):
         in_port = ports.TextStreamPort(io.StringIO("abc;"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "abc")
 
     def test_quote(self):
         in_port = ports.TextStreamPort(io.StringIO("' ( a b c )"))
-        core.write_to(reader.read(in_port), self.port)
+        core.write_to(self.reader.read(in_port), self.port)
         self.assertEqual(self.stream.getvalue(), "'(a b c)")
 
     def test_true(self):
         in_port = ports.TextStreamPort(io.StringIO("#t"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIs(result, True)
 
     def test_false(self):
         in_port = ports.TextStreamPort(io.StringIO("#f()"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIs(result, False)
 
     def test_symbol_with_digits(self):
         in_port = ports.TextStreamPort(io.StringIO("123abc"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "123abc")
 
     def test_num(self):
         in_port = ports.TextStreamPort(io.StringIO("123"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertEqual(result, 123)
 
     def test_plus_num(self):
         in_port = ports.TextStreamPort(io.StringIO("+45"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertEqual(result, 45)
 
     def test_minus_num(self):
         in_port = ports.TextStreamPort(io.StringIO("-23"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertEqual(result, -23)
 
     def test_zero_num(self):
         in_port = ports.TextStreamPort(io.StringIO('0'))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertEqual(result, 0)
 
     def test_plus(self):
         in_port = ports.TextStreamPort(io.StringIO("+"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "+")
 
     def test_minus(self):
         in_port = ports.TextStreamPort(io.StringIO("-"))
-        result = reader.read(in_port)
+        result = self.reader.read(in_port)
         self.assertIsInstance(result, core.Symbol)
         self.assertEqual(result.name, "-")
 
 
 class TestReaderError(unittest.TestCase):
 
+    def setUp(self):
+        self.reader = reader.Reader()
+
     def test_broken_list(self):
         in_port = ports.TextStreamPort(io.StringIO("("))
         with self.assertRaises(exceptions.ReaderError):
-            reader.read(in_port)
+            self.reader.read(in_port)
 
     def test_broken_string(self):
         in_port = ports.TextStreamPort(io.StringIO('"q'))
         with self.assertRaises(exceptions.ReaderError):
-            reader.read(in_port)
+            self.reader.read(in_port)
 
     def test_right_bracket(self):
         in_port = ports.TextStreamPort(io.StringIO(")"))
         with self.assertRaises(exceptions.ReaderError):
-            reader.read(in_port)
+            self.reader.read(in_port)
