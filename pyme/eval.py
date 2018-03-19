fb2b5453ce8425ceb115b04d19c6a225058784e6
Dmitry Grigoryev <dmitriy57@gmail.com>
2018-03-19 00:17:11 -0500

    Implemented non-recursive eval

--- pyme/eval.py
+++ pyme/eval.py
@@ -1,25 +1,98 @@
+from abc import ABC, abstractmethod
 import numbers
 
 from pyme import base, exceptions, interop
 
 
-def eval(expr, env):
-    if isinstance(expr, numbers.Integral):
-        return expr
-    elif isinstance(expr, str):
-        return expr
-    elif base.symbolp(expr):
-        return env[expr]
-    elif base.pairp(expr):
-        args, rest = interop.from_scheme_list(expr.cdr)
-        if not base.nullp(rest):
-            raise exceptions.EvalError("Expected proper list in procedure application")
-        proc = eval(expr.car, env)
-        args = [eval(x, env) for x in args]
-        return apply(proc, args)
-    else:
-        raise TypeError("Cannot eval type " + type(expr).__name__)
+class Result:
+
+    def __init__(self):
+        self.values = None
 
 
-def apply(proc, args):
-    return proc(*args)
+class Continuation(ABC):
+
+    def __init__(self, *, result=None):
+        self._result = result or Result()
+
+    @abstractmethod
+    def eval(self, evaluator): pass
+
+    @property
+    def result(self):
+        return self._result
+
+
+class EvalContinuation(Continuation):
+
+    def __init__(self, expr, *, result=None):
+        super().__init__(result=result)
+        self.expr = expr
+
+    def eval(self, evaluator):
+        if isinstance(self.expr, numbers.Integral):
+            self.result.values = [self.expr]
+        elif isinstance(self.expr, str):
+            self.result.values = [self.expr]
+        elif base.symbolp(self.expr):
+            value = evaluator.env[self.expr]
+            self.result.values = [value]
+        elif base.pairp(self.expr):
+            proc = self.expr.car
+            args, rest = interop.from_scheme_list(self.expr.cdr)
+            if not base.nullp(rest):
+                raise exceptions.EvalError("Expected proper list in procedure application")
+            proc_cont = EvalContinuation(proc)
+            arg_conts = [EvalContinuation(arg) for arg in args]
+            proc_result = proc_cont.result
+            arg_results = [arg_cont.result for arg_cont in arg_conts]
+            apply_cont = ApplyContinuation(proc_result, arg_results, result=self.result)
+            evaluator.push_continuation(apply_cont)
+            evaluator.push_continuations(arg_conts)
+            evaluator.push_continuation(proc_cont)
+        else:
+            raise TypeError("Cannot eval type " + type(self.expr).__name__)
+
+
+class ApplyContinuation(Continuation):
+
+    def __init__(self, proc_result, arg_results, *, result=None):
+        super().__init__(result=result)
+        self.proc_result = proc_result
+        self.arg_results = arg_results
+
+    def eval(self, evaluator):
+        proc = self.proc_result.values[0]
+        args = [arg.values[0] for arg in self.arg_results]
+        result = proc(*args)
+        self.result.values = [result]
+
+
+class Evaluator:
+
+    def __init__(self, *, env):
+        self._continuations = []
+        self.env = env
+
+    def eval(self, expr):
+        while self._continuations:
+            continuation = self._continuations.pop()
+            continuation.run(self)
+
+    def push_continuation(self, continuation):
+        self._continuations.append(continuation)
+
+
+    def push_continuations(self, continuations):
+        self._continuations.extend(continuations)
+
+
+def eval(expr, *, env):
+    evaluator = Evaluator(env=env)
+    continuation = EvalContinuation(expr)
+    result = continuation.result
+    evaluator.push_continuation(continuation)
+    while evaluator._continuations:
+        continuation = evaluator._continuations.pop()
+        continuation.eval(evaluator)
+    return result.values[0]
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyme import eval, exceptions, interop, base, reader, types
+from pyme import eval, interop, base, reader, types
 
 
 class TestEval(unittest.TestCase):
@@ -12,18 +12,27 @@ class TestEval(unittest.TestCase):
         symbol_table = types.SymbolTable()
         env = types.Environment(bindings={symbol_table["a"]: 5})
         expr = interop.read_str("a", symbol_table=symbol_table)
-        result = eval.eval(expr, env)
+        result = eval.eval(expr, env=env)
         self.assertEqual(result, 5)
 
     def test_number(self):
         env = types.Environment()
         expr = interop.read_str("3")
-        result = eval.eval(expr, env)
+        result = eval.eval(expr, env=env)
         self.assertEqual(result, 3)
 
     def test_fun_call(self):
         symbol_table = types.SymbolTable()
         env = types.Environment(bindings={symbol_table["+"]: base.plus})
         expr = interop.read_str("(+ 1 2)", symbol_table=symbol_table)
-        result = eval.eval(expr, env)
+        result = eval.eval(expr, env=env)
         self.assertEqual(result, 3)
+
+    def test_deep_recursive(self):
+        n = 100
+        string = "(+ 1 "*n + ")"*n
+        symbol_table = types.SymbolTable()
+        env = types.Environment(bindings={symbol_table["+"]: base.plus})
+        expr = interop.read_str(string, symbol_table=symbol_table)
+        result = eval.eval(expr, env=env)
+        self.assertEqual(result, n)
