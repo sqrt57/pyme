4642e9988626d35f060893260c2e34e95564a919
Dmitry Grigoryev <dmitriy57@gmail.com>
2018-04-13 17:20:59 -0500

    Refactor

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -5,7 +5,9 @@ from pyme import Interpreter
 
 class TestInterpreter(unittest.TestCase):
 
+    def setUp(self):
+        self.interpreter = Interpreter()
+
     def test_create(self):
-        interpreter = Interpreter()
-        result = interpreter.eval_str("(+ 1 2)")
+        result = self.interpreter.eval_str("(+ 1 2)")
         self.assertEqual(result, 3)
